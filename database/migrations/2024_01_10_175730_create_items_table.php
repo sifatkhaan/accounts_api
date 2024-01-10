d0e5973902a2ad2e0e981213d7a24e43ab171bb4
sifatkhan9330@gmail.com <sifatkhan9330@gmail.com>
2024-01-11 01:04:10 +0600

    first edited code

---
+++ database/migrations/2024_01_10_175730_create_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->char('name',50);
+            $table->decimal('price',10,2);
+            $table->char('uom');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
